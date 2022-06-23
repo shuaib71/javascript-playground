cbcb2f8c2b070592e20d6e065c017740023dc8a9
SHUAIB <84271086+shuaib71@users.noreply.github.com>
2022-06-23 11:30:38 +0530

    Commit by Mr.SHUAIB

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 window.onload = () => {
-    //console.log('This is your first program executing!');
+    console.log('This is your first program executing!');
+    // This is my first file to clone form github and then push back to on github.
 }
